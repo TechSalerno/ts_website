411a135c8a559fc655b571e1e199bb362476cc78
Dario Di Pasquale <darionovantadue@gmail.com>
2019-12-24 12:40:37 +0100

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+printf "\033[0;32mDeploying updates to GitHub...\033[0m\n"
+
+hugo
+cd public
+git add .
+# Commit changes.
+msg="rebuilding site $(date)"
+if [ -n "$*" ]; then
+	msg="$*"
+fi
+git commit -m "$msg"
+
+# Push source and build repos.
+git push origin master
